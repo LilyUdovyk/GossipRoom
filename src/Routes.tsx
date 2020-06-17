75d106043bdb2b48652b65aec1a68e68868da817
Lily Udovyk <pelinknew@gmail.com>
2020-06-17 20:45:38 +0300

    lazyLoading

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,12 +3,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { IRootState } from './store/rootReducer';
-import Registration from './components/Registration';
-import Authorization from './components/Authorization';
-import Profile from './components/Profile';
-import CreationNewChat from './components/CreationNewChat';
-import UserSettings from './components/UserSettings'
-import ChatSettings from './components/ChatSettings'
+
+const Registration = React.lazy(() => import("./components/Registration"));
+const Authorization = React.lazy(() => import("./components/Authorization"));
+const Profile = React.lazy(() => import("./components/Profile"));
+const CreationNewChat = React.lazy(() => import("./components/CreationNewChat"));
+const UserSettings = React.lazy(() => import("./components/UserSettings"));
+const ChatSettings = React.lazy(() => import("./components/ChatSettings"));
 
 
 const mapStateToProps = (state: IRootState) => ({
@@ -18,16 +19,18 @@ const mapStateToProps = (state: IRootState) => ({
 type RoutesProps = ReturnType<typeof mapStateToProps>
 
 const Routes: React.FC<RoutesProps> = ({ authToken }) => (
-    <Switch>
-        <Route exact path="/sign-in" component={Authorization} />
-        <Route exact path="/registration" component={Registration} />
-        {!authToken && <Redirect from="/" to="/sign-in" />}
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/new-group" component={CreationNewChat} />
-        <Route exact path="/user-settings" component={UserSettings} />
-        <Route exact path="/chat-settings" component={ChatSettings} />
-        {authToken && <Redirect from="/" to="/profile" />}
-        <Route path="/" render={() => <p>Page Not Found</p>} />
-    </Switch>
+    <React.Suspense fallback={<div>Загрузка...</div>}>
+        <Switch>
+            <Route exact path="/sign-in" component={Authorization} />
+            <Route exact path="/registration" component={Registration} />
+            {!authToken && <Redirect from="/" to="/sign-in" />}
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/new-group" component={CreationNewChat} />
+            <Route exact path="/user-settings" component={UserSettings} />
+            <Route exact path="/chat-settings" component={ChatSettings} />
+            {authToken && <Redirect from="/" to="/profile" />}
+            <Route path="/" render={() => <div>Page Not Found</div>} />
+        </Switch>
+    </React.Suspense>
 )
 export default connect(mapStateToProps)(React.memo(Routes))
