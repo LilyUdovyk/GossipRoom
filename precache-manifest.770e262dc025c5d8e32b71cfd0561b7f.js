cb86df9fcafc8f6aedb013fb38c1a1865cbd0025
Lily Udovyk <pelinknew@gmail.com>
2020-07-30 20:15:54 +0300

    Updates

---
+++ precache-manifest.770e262dc025c5d8e32b71cfd0561b7f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e58de032187a8449af78a2f1ae30a69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "251ca9a45eb5bbe92d51",
+    "url": "./static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "5f6a599d9e46ec2e135f",
+    "url": "./static/css/4.adbe3ef3.chunk.css"
+  },
+  {
+    "revision": "a97d8b56c82d727712d5",
+    "url": "./static/css/5.6ea2bd08.chunk.css"
+  },
+  {
+    "revision": "c23a8d3acab595341e24",
+    "url": "./static/css/6.b767fe06.chunk.css"
+  },
+  {
+    "revision": "c10005b5b86c79bd6464",
+    "url": "./static/css/7.a9556f5c.chunk.css"
+  },
+  {
+    "revision": "b628477177cd5a62a018",
+    "url": "./static/css/8.42b503f8.chunk.css"
+  },
+  {
+    "revision": "77dac476d16ad96759bb",
+    "url": "./static/css/9.ce60c5de.chunk.css"
+  },
+  {
+    "revision": "2ffe0b9780e5314f8a38",
+    "url": "./static/css/main.2780051b.chunk.css"
+  },
+  {
+    "revision": "251ca9a45eb5bbe92d51",
+    "url": "./static/js/2.68c4f27c.chunk.js"
+  },
+  {
+    "revision": "41532a40b82eda92a937cc40e73ab01e",
+    "url": "./static/js/2.68c4f27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f3e4016ad9c9a79e7d",
+    "url": "./static/js/3.4e79ee43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/3.4e79ee43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6a599d9e46ec2e135f",
+    "url": "./static/js/4.e1a17342.chunk.js"
+  },
+  {
+    "revision": "a97d8b56c82d727712d5",
+    "url": "./static/js/5.d42565b4.chunk.js"
+  },
+  {
+    "revision": "c23a8d3acab595341e24",
+    "url": "./static/js/6.e34b6755.chunk.js"
+  },
+  {
+    "revision": "c10005b5b86c79bd6464",
+    "url": "./static/js/7.35c0193c.chunk.js"
+  },
+  {
+    "revision": "b628477177cd5a62a018",
+    "url": "./static/js/8.7addf600.chunk.js"
+  },
+  {
+    "revision": "77dac476d16ad96759bb",
+    "url": "./static/js/9.e1f1e4f3.chunk.js"
+  },
+  {
+    "revision": "2ffe0b9780e5314f8a38",
+    "url": "./static/js/main.f6bf5090.chunk.js"
+  },
+  {
+    "revision": "e4c3dd7ada3b15fa0fb3",
+    "url": "./static/js/runtime-main.196cd8b0.js"
+  },
+  {
+    "revision": "c3631c652abe1185b1874da24af0b7c7",
+    "url": "./static/media/user_avatar.c3631c65.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.83eae018b58e02deee13b01612800b2c.js"
+  "./precache-manifest.770e262dc025c5d8e32b71cfd0561b7f.js"
 );
 
 self.addEventListener('message', (event) => {
